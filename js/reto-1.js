e8cf291aa93e006749a5bf2f2a732004102d8437
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-05-11 10:50:07 +0200

    Mis cambios

---
+++ js/reto-1.js
@@ -0,0 +1,9 @@
+let letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+
+function imprimirlongitudDeLista(lista) {
+  return lista.length;
+}
+
+const longitudDeLetras = imprimirlongitudDeLista(letras);
+
+console.log(longitudDeLetras);

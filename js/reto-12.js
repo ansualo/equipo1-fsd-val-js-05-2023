70e177370a7448be4e6409237d6699d2a2df2330
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-05-11 13:54:12 +0200

    3 formas finas de recorrer arrays

--- js/reto-12.js
+++ js/reto-12.js
@@ -3,26 +3,38 @@
 // las propiedades para todos los objetos.
 
 let personas = [
-    {
-        nombre: "Andrea",
-        apellido: "Suarez",
-        edad: "31",
-        pais: "España"
-    },
-    {
-        nombre: "Edu",
-        apellido: "Kiriakos",
-        edad: "26",
-        pais: "Venezuela"
-    },
-    {
-        nombre: "Judit",
-        apellido: "Grau",
-        edad: "33",
-        pais: "España"
-    }
+  {
+    nombre: "Andrea",
+    apellido: "Suarez",
+    edad: "31",
+    pais: "España",
+  },
+  {
+    nombre: "Edu",
+    apellido: "Kiriakos",
+    edad: "26",
+    pais: "Venezuela",
+  },
+  {
+    nombre: "Judit",
+    apellido: "Grau",
+    edad: "33",
+    pais: "España",
+  },
 ];
 
-const imprimirNombre= (nombreDef)=>{
-    console.log (personas)
-};
+// Esta la la forma chimba
+// const imprimirNombre= (array)=>{
+//     for(let i = 0; i < personas.length; i++) {
+//         console.log(array[i].nombre)
+//     }
+// };
+
+// Esta también está guay
+// personas.forEach((persona) => {
+//     console.log(persona.nombre);
+// })
+
+personas.forEach(({ nombre }) => {
+    console.log(nombre);
+})
